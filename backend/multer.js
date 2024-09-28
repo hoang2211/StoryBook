c1ea98da4d9e97d50d24d55be4550f16db9afa22
ZoekLis <hoangnntde170445@fpt.edu.vn>
2024-09-28 17:26:31 +0700

    addst

---
+++ backend/multer.js
@@ -0,0 +1,27 @@
+const multer = require("multer");
+const path = require("path");
+
+//Storage configuaration
+const storage = multer.diskStorage({
+    destination: function(req, file,cb){
+        cb(null,"./uploads/");
+    },
+    filename: function(req, file, cb){
+        cb(null,Date.now() + path.extname(file.originalname));
+    },
+});
+
+//File filter
+const fileFilter = (req,file,cb)=>{
+    if(file.mimetype.startsWith("image/")){
+        cb(null, true);
+    }else{
+        cb(new Error("Only images are allowed"), false);
+    }
+};
+
+//Initialize multer instance
+
+const upload = multer({storage, fileFilter});
+
+module.exports = upload;
